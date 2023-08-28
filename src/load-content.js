8a1bbd865fc7f1b66f3e1ee782d40b33cf513254
MostafaSaad1987 <52860974+MostafaSaad1987@users.noreply.github.com>
2023-08-28 21:32:26 +0300

    bare-bones site and setup

---
+++ src/load-content.js
@@ -0,0 +1,9 @@
+/*
+Now remove those elements from the HTML
+(so leave only the <html>, <body>, and <div id="content"> tags)
+and instead create them by using JavaScript only,
+e.g. by appending each new element to div#content once the page is first loaded.
+
+Since we’re all set up to write our code in multiple files,
+let’s write this initial page-load function inside of its own module and then import and call it inside of index.js.
+*/
