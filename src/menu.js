9036076f44861f4e66e573b09dce1999b923a530
MostafaSaad1987 <52860974+MostafaSaad1987@users.noreply.github.com>
2023-09-01 07:19:53 +0300

    Added text to describe the food.

--- src/menu.js
+++ src/menu.js
@@ -1,12 +1,12 @@
 const contentDiv = document.querySelector("#content");
 
 const foodAvailable = [
-    { name: 'Puddings', link: 'https://static.wikia.nocookie.net/dragonball/images/a/ae/Puddings.png/revision/latest?cb=20201120020120' },
-    { name: 'Paradise herb', link: 'https://static.wikia.nocookie.net/dragonball/images/d/d8/Paradise_herb.jpg/revision/latest?cb=20180826194338' },
-    { name: 'Ramen', link: 'https://static.wikia.nocookie.net/dragonball/images/7/77/RamenDBS.png/revision/latest?cb=20201120020507' },
-    { name: 'Fruit of the Tree of Might', link: 'https://static.wikia.nocookie.net/dragonball/images/7/7a/Dbz_movie3_1193.jpg/revision/latest?cb=20180522175046' },
-    { name: 'Eggs', link: 'https://static.wikia.nocookie.net/dragonball/images/c/ce/PteroEggs.png/revision/latest?cb=20111227002822' },
-    { name: 'Fancy Chocolates', link: 'https://static.wikia.nocookie.net/dragonball/images/f/f4/Dbz237_-_by_%28dbzf.ten.lt%29_20120329-16570146.jpg/revision/latest?cb=20120331090659' }
+    { name: 'Pudding', link: 'https://static.wikia.nocookie.net/dragonball/images/a/ae/Puddings.png/revision/latest?cb=20201120020120', description: "Almost caused the universe to be destroyed from how good it is." },
+    { name: 'Paradise herb', link: 'https://static.wikia.nocookie.net/dragonball/images/d/d8/Paradise_herb.jpg/revision/latest?cb=20180826194338', description: "Increase your life span by 1,000 years, but you'd have to live on an island alone with a talking turtle." },
+    { name: 'Ramen', link: 'https://static.wikia.nocookie.net/dragonball/images/7/77/RamenDBS.png/revision/latest?cb=20201120020507', description: "Good ol' ramen. Most common food, but tasets especially good from Kooky Cook." },
+    { name: 'Fruit of the Tree of Might', link: 'https://static.wikia.nocookie.net/dragonball/images/7/7a/Dbz_movie3_1193.jpg/revision/latest?cb=20180522175046', description: "Fills you with energy that is extracted from an entire planet." },
+    { name: 'Eggs', link: 'https://static.wikia.nocookie.net/dragonball/images/c/ce/PteroEggs.png/revision/latest?cb=20111227002822', description: "Chicken eggs, Dragon eggs, Demon King eggs, Cell eggs, Magic eggs, you name it." },
+    { name: 'Fancy Chocolates', link: 'https://static.wikia.nocookie.net/dragonball/images/f/f4/Dbz237_-_by_%28dbzf.ten.lt%29_20120329-16570146.jpg/revision/latest?cb=20120331090659', description: "Totally not poisonous." }
 ];
 
 const AddMenuItems = () => {
@@ -32,7 +32,15 @@ const AddMenuItems = () => {
         newImg.classList.add("food-image");
         newImg.src = imgLink;
 
-        newFoodCard.append(newImg, newP);
+        let overlayElement = document.createElement("div");
+        overlayElement.classList.add("overlay");
+
+        let foodDescription = document.createElement("div");
+        foodDescription.classList.add("food-description");
+        foodDescription.textContent = food.description;
+
+        overlayElement.append(foodDescription);
+        newFoodCard.append(newImg, newP, overlayElement);
         menuContent.append(newFoodCard);
     }
 
